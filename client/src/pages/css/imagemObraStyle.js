6fe189c32acac03b7493f6168284ed155ff6f78c
Pedro Lara <pedroalveslara@gmail.com>
2022-07-15 16:47:58 -0300

    Update imagemObraStyle.js

--- client/src/pages/css/imagemObraStyle.js
+++ client/src/pages/css/imagemObraStyle.js
@@ -6,7 +6,7 @@ const imagemObraStyle = {
     border: '.3rem solid rgba(169, 210, 215, 1)',
     borderRadius: '1rem',
     margin: '2rem',
-    width: '5rem'
+    width: '6rem'
 }
 
 export default imagemObraStyle
