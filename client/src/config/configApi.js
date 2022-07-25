fe5cc8c61037949e414a744fb25ae2575b6c147e
Pedro Lara <pedroalveslara@gmail.com>
2022-07-25 09:02:59 -0300

    Update configApi.js

--- client/src/config/configApi.js
+++ client/src/config/configApi.js
@@ -3,11 +3,11 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://192.168.10.122:1212/" // FIXME TO IP SERVER
+    baseURL: "http://192.168.10.127:1212/" // FIXME TO IP SERVER
 });
 
 export const api = axios.create({
-    baseURL:"http://192.168.10.122:1212/" // FIXME TO IP SERVER
+    baseURL:"http://192.168.10.127:1212/" // FIXME TO IP SERVER
 });
 
 export const createSession = async (usuario, senha) => {
